7ef1bc0610fef33a1a34e5dbe20a8983d0d7e055
ivettepinto <ivette.pinto@telusinternational.com>
2023-02-09 21:16:31 -0600

    Add: Dice class for characters throwing dices

---
+++ Game/Dice.java
@@ -0,0 +1,12 @@
+package Game;
+
+import java.util.Random;
+
+public class Dice {
+    private static Random rn = new Random();
+
+    public static int throwDice(int limit){
+        return rn.nextInt(limit);
+    }
+    
+}

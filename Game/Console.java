4192c864b8a7b8efcd3477ae28d6f2e0df2b9276
ivettepinto <ivette.pinto@telusinternational.com>
2023-02-09 20:29:15 -0600

    Add: Console class for inputs

---
+++ Game/Console.java
@@ -0,0 +1,29 @@
+package Game;
+
+import java.util.Scanner;
+
+public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int getInt(){
+        int responseInt = 0;
+        boolean error = false;
+        do{
+            try {
+                responseInt = scanner.nextInt();
+                error = false;
+            } catch (Exception e) {
+                error = true;
+            }
+        }while(error);
+        return responseInt;
+    }
+
+    public static String getString(){
+        return scanner.nextLine();
+    }
+
+    public static String getStringNext(){
+        return scanner.next();
+    }
+}

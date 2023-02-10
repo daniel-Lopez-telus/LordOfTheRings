8d7e50440594e96710045f416d9261dad5c24f95
Daniel Lopez <daniel.lopez09@telusinternational.com>
2023-02-09 22:02:59 -0600

    AbstractCharacterFactory class fixed

--- Game/characterFactory/AbstractCharacterFactory.java
+++ Game/characterFactory/AbstractCharacterFactory.java
@@ -2,5 +2,5 @@ package Game.characterFactory;
 
 public interface AbstractCharacterFactory {
     
-    public Game.characters.Character createCharacter(String type);
+    public Character createCharacter(String type);
 }

3c9047fbbc3e3e8889fde40386a522765a4c4356
Daniel Lopez <daniel.lopez09@telusinternational.com>
2023-02-12 16:16:00 -0600

    removed methods out of use

--- Game/Turn.java
+++ Game/Turn.java
@@ -9,24 +9,18 @@ public class Turn {
     /**
      * NOTAS:
      * 1. QUE VA A SUCEDER SI TENEMOS UN CREATURE MUERTO EN CUALQUIER ARMY? ENTONCES EL CREATURE DEL OTRO LADO YA NO PELEA?
-     * posibles respuestas: 
-     * 1.1 en cada turno un personaje de un ejercito atacara a un unico adversario del ejercito oponente. 
+     * R/ en cada turno un personaje de un ejercito atacara a un unico adversario del ejercito oponente. 
      * se enfrentaran siempre los personajes situados en la misma posicion de cada ejercito. Si alguno de los ejercitois
      * dispone de mas efectivos que el contrario los personajes sobrantes no participaran en ese turno de batalla. 
      * 
      * 
      * 2. DEFINIR LA CLASE COMBATHISTORY... PUEDE SER LA QUE VAYA IMPRIMIENDO LOS MENSAJES EN CONSOLA EN CADA TURNO PERO ES 
      * SOLAMENTE UNA IDEA!!
-     * 3. EL METODO DEFEND DE LA CLASE CREATURE ES PROBABLE QUE YA NO SEA MAS UTIL, NUNCA LA USE. ESTUDIAR METODO COMBAT :)
-     * 4. ECHAR OJO A LA CONDICION beast.getCharacterType() == CCreatures.Types.ORC.ordinal() DE LA LINEA 61.
      */
     
     //los ejercitos son clases que contienen arreglos de heroes y bestias
     private Army heroeArmy;
     private Army beastArmy;
-    private int heroeAttack;
-    private int beastAttack;
-    private int damageDealt;
     private CombatHistory combatHistory;
 
     public Turn(Army heroeArmy, Army beastArmy) {
@@ -40,7 +34,7 @@ public class Turn {
         Creature heroeToBattle;
         Creature beastToBattle;
         boolean isAnyArmyAlive = true;
-        
+
         while (isAnyArmyAlive) {
             for (Creature heroe : heroeArmy.getArmy()) {
                 if (heroe.getLifePoints() <= 0) {
@@ -107,12 +101,6 @@ public class Turn {
             }else{
                 return false;
             }
-    }
-
-    // este metodo me quedo sin usar
-    private int calculateDamageDealt(){
-        //TODO
-        return 0;
     }
 
     //definir que va hacer nuestro combatHistory y hacer un display por cada turno como en el ejemplo... solo son ideas de mi cabeza. metodo por definir

4f37e6b88846a2c673455cf7b5ff8a301e41580e
Daniel Lopez <daniel.lopez09@telusinternational.com>
2023-02-27 12:20:34 -0600

    game moved-on to /src

--- src/main/java/com/lotr/App.java
+++ src/main/java/com/lotr/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LordOfTheRings myGame = new LordOfTheRings();
+        myGame.setup();
+        myGame.execute();
     }
 }
---
+++ src/main/java/com/lotr/Army.java
@@ -0,0 +1,31 @@
+package com.lotr;
+
+import java.util.ArrayList;
+import com.lotr.creatures.Creature;
+
+public class Army {
+    
+    private String armyName;
+    ArrayList<Creature> army;
+
+    public Army(String name, int size){
+        this.armyName = name;
+        this.army = new ArrayList<>(size);
+    }
+
+    public void insertIntoArmy(Creature creature){
+        army.add(creature);
+    }
+
+    public ArrayList<Creature> getArmy() {
+        return army;
+    }
+
+    public int getArmySize() {
+        return this.army.size();
+    }
+
+    public String getArmyName() {
+        return this.armyName;
+    }
+}
